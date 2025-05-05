039a7a424da56f8363067cb866ce5df024e54cbf
Devanand Yadav <devanand@brahmandx.com>
2025-05-05 15:01:54 +0530

    website file added

---
+++ script.js
@@ -0,0 +1,9 @@
+// Call to action button interaction
+document.getElementById('cta-button').addEventListener('click', function () {
+    alert('Welcome to BHXpay! Let’s start your journey.');
+});
+
+// Live Chat Button Interaction
+function startChat() {
+    alert('You can chat with our support team!');
+}
